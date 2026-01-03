a8d0c7b3ddd59d36edde300894919c5e6d1a61c4
vmaslyhan <138622151+vmaslyhan@users.noreply.github.com>
2026-01-03 17:11:25 +0200

    Delete app directory

--- app/django_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from hotel.views import index
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
-]
--- app/hotel/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Guest, Room, Registration
-
-admin.site.register(Guest)
-admin.site.register(Room)
-admin.site.register(Registration)
